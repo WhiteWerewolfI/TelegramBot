c9f5d37eff75e18835132e55a040abfbda114cf6
Andrey Bethann <andreybe@student.21-school.ru>
2025-07-31 00:59:40 +0300

    first commit

---
+++ src/test/java/ru/andreybe/SpringDemoBot/SpringDemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andreybe.SpringDemoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
